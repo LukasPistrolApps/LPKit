596f10704fb0566919197884bf7a314826f03a8b
Lukas Pistrol <lukas@pistrol.com>
2022-11-21 09:32:14 +0100

    add filename param to JSONExporter

--- Sources/LPJSONPicker/LPJSONExporter.swift
+++ Sources/LPJSONPicker/LPJSONExporter.swift
@@ -12,9 +12,10 @@ public extension View {
     func jsonExporter<T: Encodable>(
         isPresented: Binding<Bool>,
         data: [T],
+        fileName: String,
         onCompletion: @escaping (Result<URL, Error>) -> Void
     ) -> some View {
-        modifier(JSONExporter(isPresented: isPresented, data: data, onCompletion: onCompletion))
+        modifier(JSONExporter(isPresented: isPresented, data: data, fileName: fileName, onCompletion: onCompletion))
     }
 }
 
@@ -24,9 +25,13 @@ struct JSONExporter<T: Encodable>: ViewModifier {
     @Binding private var isPresented: Bool
     var completion: (Result<URL, Error>) -> Void
 
-    init(isPresented: Binding<Bool>, data: [T], onCompletion: @escaping (Result<URL, Error>) -> Void) {
+    init(isPresented: Binding<Bool>,
+         data: [T],
+         fileName: String,
+         onCompletion: @escaping (Result<URL, Error>) -> Void
+    ) {
         self._isPresented = isPresented
-        self._model = .init(wrappedValue: .init(data: data))
+        self._model = .init(wrappedValue: .init(data: data, fileName: fileName))
         self.completion = onCompletion
     }
 
@@ -42,9 +47,11 @@ struct JSONExporter<T: Encodable>: ViewModifier {
 class LPJSONExporterViewModel<T: Encodable>: ObservableObject {
     @Published var data: [T]
     @Published var document: JSONFile?
+    var fileName: String
 
-    public init(data: [T]) {
+    public init(data: [T], fileName: String) {
         self.data = data
+        self.fileName = fileName
         createFile()
     }
 
@@ -53,7 +60,7 @@ class LPJSONExporterViewModel<T: Encodable>: ObservableObject {
             let data = try JSONEncoder().encode(data)
             let json = try JSONSerialization.jsonObject(with: data)
             let jsonData = try JSONSerialization.data(withJSONObject: json, options: .prettyPrinted)
-            self.document = JSONFile(data: jsonData)
+            self.document = JSONFile(data: jsonData, fileName: fileName)
         } catch {
             print(error)
         }
@@ -64,13 +71,16 @@ struct JSONFile: FileDocument {
     static var readableContentTypes: [UTType] = [.json]
 
     var text = ""
+    var fileName: String?
 
-    init(text: String = "") {
+    init(text: String = "", fileName: String) {
         self.text = text
+        self.fileName = fileName
     }
 
-    init(data: Data) {
+    init(data: Data, fileName: String) {
         self.text = String(decoding: data, as: UTF8.self)
+        self.fileName = fileName
     }
 
     init(configuration: ReadConfiguration) throws {
@@ -81,6 +91,10 @@ struct JSONFile: FileDocument {
 
     func fileWrapper(configuration: WriteConfiguration) throws -> FileWrapper {
         let data = Data(text.utf8)
-        return FileWrapper(regularFileWithContents: data)
+        let fileWrapper = FileWrapper(regularFileWithContents: data)
+        if let fileName {
+            fileWrapper.preferredFilename = fileName
+        }
+        return fileWrapper
     }
 }
